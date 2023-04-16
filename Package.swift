f480bc14b3bff7e4e2e8f55c06bd7e55551ad81f
Сергей Гаврилов <o.s.t.gavr.sg@gmail.com>
2023-04-17 00:26:58 +0300

    secp256k1.swift

--- Package.swift
+++ Package.swift
@@ -14,14 +14,16 @@ let package = Package(
         .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.8.4"),
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.0"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.0.0"),
+        .package(url: "https://github.com/Boilertalk/secp256k1.swift", from: "0.1.7"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "secp256k1Local"),
+//        .target(name: "secp256k1Local"),
         .target(
             name: "web3swift",
-            dependencies: ["BigInt", "secp256k1Local", "PromiseKit", "Starscream", "CryptoSwift"],
+            dependencies: ["BigInt", "PromiseKit", "Starscream", "CryptoSwift",
+                           .product(name: "secp256k1", package: "secp256k1.swift"),],
             exclude: [
             ]),
         .testTarget(
